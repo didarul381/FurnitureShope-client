44e8a8381869c970f0eed77038f959b19e6c476d
didarul381 <didarula128@gmail.com>
2022-11-28 02:49:15 -0800

    Admin route added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-primary '>
-            <h2 className='text-center text-2xl p-4 m-3 '>Copy Right FurniShop || 2022</h2>
+        <div className=''>
+            <h2 className='text-center text-2xl p-4 m-3 text-light '>Copy Right FurniShop || 2022</h2>
         </div>
     );
 };
