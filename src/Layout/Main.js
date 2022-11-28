ca68df110fbf7deb543077b8f6d0f191c7bbb570
didarul381 <didarula128@gmail.com>
2022-11-28 02:38:37 -0800

    All users added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Footer } from 'react-day-picker';
 import { Outlet } from 'react-router-dom';
 import Navbar from "../Pages/Shared/Navbar/Navbar"
-//import Footer from "../Pages/Shared/Footer/Footer"
+
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
             
         </div>
     );
